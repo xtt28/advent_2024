e4a3289045df08dd5ae1421280acb5d65b2f8eb6
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-12-02 06:57:19 -0500

    Day 2 iteration 2

--- Day2/day2.py
+++ Day2/day2.py
@@ -4,19 +4,27 @@ with open("input.txt", "r") as f:
 
 safe_lines = 0
 
-for line in lines:
-    nums = [int(n) for n in line.split()]
-    bad = False
+def is_safe(nums):
     if sorted(nums) != nums and sorted(nums)[::-1] != nums:
-        bad = True
-        continue
+        return False
     
     for i in range(len(nums) - 1):
         dist = abs(nums[i] - nums[i+1])
         if dist < 1 or dist > 3:
-            bad = True
+            return False
     
-    if not bad:
+    return True
+
+
+for line in lines:
+    nums = [int(n) for n in line.split()]
+    if is_safe(nums):
         safe_lines += 1
+    else:
+        for i in range(len(nums)):
+            if is_safe(nums[0:i-1] + nums[i:]):
+                safe_lines += 1
+                continue
+    
 
 print(f"Safe reports: {safe_lines}")
