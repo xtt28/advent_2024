12e4a9f8cf48c174e7d653516f380f1da8078921
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-12-03 07:46:10 -0500

    day 3 iter 2

--- Day3/day3.py
+++ Day3/day3.py
@@ -2,12 +2,18 @@ import re
 
 with open("input.txt", "r") as f:
     global data
-    data = f.read()
+    data = f.read().split("do()")
 
 sum_prods = 0
 
-for res in re.findall(r"mul\([0-9]*,[0-9]*\)", data):
-    eq = res.replace("mul(", "").replace(",", "*").replace(")", "")
-    sum_prods += eval(eq)
+def evaluate_all_mul(s):
+    global sum_prods
+    for res in re.findall(r"mul\([0-9]*,[0-9]*\)", s):
+        eq = res.replace("mul(", "").replace(",", "*").replace(")", "")
+        sum_prods += eval(eq)
+
+for st in data:
+    st = st.split("don't()", 1)[0]
+    evaluate_all_mul(st)
 
 print(sum_prods)
