b71e0b629d794e27721c05c57405064470d4913a
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-12-04 09:15:29 -0500

    day 4 iter 2

--- Day4/day4.py
+++ Day4/day4.py
@@ -3,36 +3,16 @@ with open("input.txt", "r") as f:
     global lines
     lines = f.readlines()
 
-SEARCH_WORDS = ["XMAS", "XMAS"[::-1]]
+WORDS = ["MAS", "MAS"[::-1]]
 count = 0
 
-for word in SEARCH_WORDS:
-    for i in range(len(lines)):
-        line = lines[i]
-        count += line.count(word)
-
-    for i in range(len(lines)):
-        for j in range(140):
-            try:
-                if lines[i][j] + lines[i+1][j] + lines[i+2][j] + lines[i+3][j]  == word:
-                    count += 1
-            except IndexError:
-                pass
-
-    for i in range(len(lines)):
-        for j in range(140):
-            try:
-                if lines[i][j] + lines[i+1][j+1] + lines[i+2][j+2] + lines[i+3][j+3] == word:
-                    count += 1
-            except IndexError:
-                pass
-
-    for i in range(len(lines)):
-        for j in range(140):
-            try:
-                if lines[i][j] + lines[i+1][j-1] + lines[i+2][j-2] + lines[i+3][j-3] == word:
-                    count += 1
-            except IndexError:
-                pass
+for i in range(len(lines)):
+    for j in range(140):
+        try:
+            if lines[i][j] + lines[i+1][j+1] + lines[i+2][j+2] in WORDS and lines[i][j+2] + lines[i+1][j+1] + lines[i+2][j] in WORDS:
+                count += 1
+        except IndexError:
+            pass
+        
 
 print(count)
