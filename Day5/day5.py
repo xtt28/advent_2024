605f1912718b360cd60623d0ed44b2145b41540d
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-12-05 07:32:19 -0500

    day 5 iter 2

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,3 +1,8 @@
+# attention
+# this code shall cause brain damage
+# read with caution
+from functools import cmp_to_key
+
 with open("input.txt", "r") as f:
     global lines
     lines = f.readlines()
@@ -5,6 +10,25 @@ with open("input.txt", "r") as f:
 befores = []
 nlfound = False
 filtered = []
+
+def is_incorrectly_ordered(s):
+    for b in befores:
+        try:
+            if s.index(b[0]) > s.index(b[1]):
+                return True
+        except ValueError:
+            continue
+    return False
+
+def cmp(a, b):
+    incord = is_incorrectly_ordered([a, b])
+    if incord:
+        return 1
+    else:
+        return -1
+
+def order(g):
+    return sorted(g, key=cmp_to_key(cmp))
 
 for line in lines:
     if line == "\n":
@@ -15,15 +39,9 @@ for line in lines:
         befores.append((s[0], s[1]))
     else:
         s = [int(x) for x in line.split(",")]
-        bad = False
-        for b in befores:
-            try:
-                if s.index(b[0]) > s.index(b[1]):
-                    bad = True
-            except ValueError:
-                continue
-        if not bad:
+        if is_incorrectly_ordered(s):
             filtered.append(s)
 
-sums = sum([s[int((len(s) - 1)/2)] for s in filtered])
+
+sums = sum([order(s)[int((len(s) - 1)/2)] for s in filtered])
 print(sums)
