c8d06653e53629a70ba93a9d1ddd99ea3df59fd4
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-12-07 08:40:45 -0500

    day 7 iter 1

---
+++ Day7/day7.py
@@ -0,0 +1,37 @@
+from itertools import product
+from math import prod
+
+with open("input.txt", "r") as f:
+    global lines
+    lines = f.readlines()
+
+operators = {"+", "*"}
+s = 0
+
+def eval_ltor(tokens):
+    initial = tokens[0]
+    op = tokens[1]
+    for token in tokens[2:]:
+        if isinstance(token, int):
+            initial = eval(f"{initial}{op}{token}")
+        else:
+            op = token
+    return initial
+
+for line in lines:
+    target = int(line.split(": ", 1)[0])
+    nums = [int(a.replace("\n", "")) for a in line.split(" ")[1:]]
+
+    perms = list(product(operators, repeat=len(nums) - 1))
+    for perm in perms:
+        expr = []
+        for i in range(len(nums)):
+            expr.append(nums[i])
+            if i < len(nums) - 1:
+                expr.append(perm[i])
+        evaluated = eval_ltor(expr)
+        if evaluated == int(target):
+            s += evaluated
+            break
+
+print(s)
